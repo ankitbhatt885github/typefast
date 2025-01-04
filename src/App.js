bb289f79ef33de55cd2689d1c9a4f61d10c4e8da
Ankit Bhatt <ankitbhatt885github@gmail.com>
2025-01-04 21:49:40 +0530

    added inout bar and basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
  
 import './App.css';
 import Header from './components/Header';
+import MainScreen from './components/MainScreen';
 
 function App() {
   return (
     <div className='App'>
 
       <Header />
+      <MainScreen />
 
     </div>
   );
--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,46 +1,68 @@
 import React, { useEffect, useState } from 'react'
-import "../stylesMainScreen.css"
+import "../styles/MainScreen.css"
 import { Words } from '../assets/Words';
 
 const MainScreen = () => {
-    const[data, setData] = useState([]);
+    const [data, setData] = useState([]);
     //2D array 
-    const [segmentedData , setSegmentedData] = useState([])
+    const [segmentedData, setSegmentedData] = useState([])
+    const[input,setInput] = useState("")
 
     //get 50 random words into an array
-    useEffect(()=>{
+    useEffect(() => {
         const fiftyWords = [];
-        for(let i=0;i<50;i++){
-            fiftyWords.push(Words[Math.floor(Math.random()*Words.length)])
+        for (let i = 0; i < 50; i++) {
+            fiftyWords.push(Words[Math.floor(Math.random() * Words.length)])
             setData(fiftyWords);
-            
+
         }
 
-    },[])
+    }, [])
     //adding 5 rows with 10 words each
-    useEffect(()=>{
-        if(data.length == 0) return;
+    useEffect(() => {
+        if (data.length === 0) return;
         const bufferSegmentedData = [];
         var counter = 0;
 
-        while(counter+10 <= data.length){
+        while (counter + 10 <= data.length) {
             const rowData = []
-            for(let i=counter;i<counter+10;i++){
+            for (let i = counter; i < counter + 10; i++) {
                 //add 10 words into rowData
                 rowData.push(data[i]);
             }
-                bufferSegmentedData.push(rowData);
-                counter +=10;
-            
+            bufferSegmentedData.push(rowData);
+            counter += 10;
+
         }
         setSegmentedData(bufferSegmentedData);
 
 
-    },[data]);
+    }, [data]);
+
+    function handleInput( ){
+
+    }
+
     
-  return (
-    <div>MainScreen</div>
-  )
+
+    return (
+        <React.Fragment>
+            <div id="main-screen">
+                {/* we get each row  */}
+                {segmentedData.map((row, rowIndex) =>
+                    <div key={rowIndex} className={rowIndex + " screen-row"} >
+                        {row.map((value, valueIndex) => <div id={valueIndex} key={valueIndex} className="display-value">{value}</div>)}
+                    </div>)
+                }
+            </div>
+            <div>
+                <input type = "text" id="main-input" placeholder="Start Typing Here" autoComplete="off" value = {input} onChange={handleInput} ></input>
+            </div>
+
+
+        </React.Fragment>
+
+    )
 }
 
 export default MainScreen
